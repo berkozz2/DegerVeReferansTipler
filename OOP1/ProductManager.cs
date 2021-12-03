55321b9a3e4e676e1609e75485054a5266d50ece
Berk OZMENEKSE <ozmenekseberk@outlook.com>
2021-12-04 00:18:17 +0300

    #NewAddedOOP1

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product) 
+        {
+            Console.WriteLine("Ürününüz Eklendi : " + product.ProductName);
+        }
+
+
+        public void BiseyYap(int sayi) 
+        {
+            sayi = 99;
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using OOP1;
+
+Product product1 = new Product();
+
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnitsInStock = 3;
+product1.ProductDescription = "Çamdan Masa";
+
+Product product2 = new Product { Id = 2, CategoryId = 3, 
+    ProductName = "Laptop", UnitPrice = 2000, UnitsInStock = 10 };
+
+//PascalCase kelimelerin ilk harfi büyük yazılır.
+//c# case sensitive'dir. büyük küçük harf duyarlıdır.
+ProductManager productManager = new ProductManager();
+
+productManager.Add(product1);
+
+//Deneme yapıldı. Kod okuma açısından
+int sayi = 100;
+productManager.BiseyYap(sayi);
+Console.WriteLine(sayi); //sonucu 100 olarak aldık. 
+
+
+
+
